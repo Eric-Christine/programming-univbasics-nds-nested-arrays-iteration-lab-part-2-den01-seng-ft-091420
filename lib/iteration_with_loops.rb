c9e9a07f312bb1d439b9c546736e8fb1c310101b
Eric Christine <ericjchristine@gmail.com>
2020-08-17 15:08:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -24,5 +24,6 @@ def find_min_in_nested_arrays(src)
   end
   
   # Above says while count is less than the array of array length. print the count index of the array of array
-  # Using p will display each array, but puts will output all the values inside those arrays! We can use p here to make the output a little clearer.
+  # Using p will display each array, but puts will output all the values inside those arrays! We can use p here to make the output a little clearer. We will want to use p for our example. 
+  
   
